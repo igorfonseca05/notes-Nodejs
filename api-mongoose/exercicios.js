39ed172d7320cbe105a0b527b1e2b62700c8c97f
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-02-09 09:33:47 -0300

    Rota entregar user image

---
+++ api-mongoose/exercicios.js
@@ -0,0 +1,25 @@
+
+class Produtos {
+    constructor(name, price, height, width) {
+        this.name = name
+        this.price = price
+        this.height = height
+        this.width = width
+    }
+
+    getProduct() {
+        console.log({ name: this.name, price: this.price, height: this.height })
+    }
+
+}
+
+const abajur = new Produtos('abajur lilas', 120.65, 56, 25)
+
+
+// Produtos para casa
+
+
+
+// Produtos para banho
+
+// Produtos para escritório
