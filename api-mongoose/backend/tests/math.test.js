eac96b4af46595d3876dd919b9c239e3b05d86a4
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-02-12 08:00:56 -0300

    add files

--- api-mongoose/backend/tests/math.test.js
+++ api-mongoose/backend/tests/math.test.js
@@ -1,4 +1,14 @@
 
-test('Olá mundo', () => {
+require('dotenv').config({ path: '.env.test' })
+
+
+// test('Deve retornar nome igor', () => {
+//     const nome = 'alan'
+//     expect(nome).toBe('igor')
+// })
+
+test('Deve retornar nome igor', () => {
+    expect(process.env.DB_URL).toBe('mongodb://127.0.0.1:27017/auth')
 
 })
+
