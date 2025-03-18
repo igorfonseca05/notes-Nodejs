51590e42ac4da14be96858c354e0c39bc68e3f80
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-03-18 11:15:26 -0300

    alterei arquivo math.js

--- api-mongoose/backend/tests/math.test.js
+++ api-mongoose/backend/tests/math.test.js
@@ -1,17 +1,15 @@
 
-require('dotenv').config({ path: '.env.test' })
-
 const { sum } = require('./math')
 
-
+// Teste 1
 test('obter soma', () => {
     const soma = sum(1, 2)
     expect(soma).toBe(3)
 })
 
 
+// Teste 2
 test('Nome deve ser igor', () => {
     const name = 'Alan'
-
     expect(name).toBe('igor')
 })
