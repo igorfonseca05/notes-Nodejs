f6280e2bdd1638bca0eaf5a5c13899ec07de78df
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-03-20 11:59:55 -0300

    alterei aquivo de test

--- api-mongoose/backend/tests/users.test.js
+++ api-mongoose/backend/tests/users.test.js
@@ -5,19 +5,46 @@ const request = require('supertest')
 const app = require('../app')
 const { default: mongoose } = require('mongoose')
 
+const User = require('../src/model/userModel')
 
-test('Deve cadastrar usuário', async () => {
-    await request(app)
-        .post('/users/signup')
-        .send({
-            userName: 'Igor',
-            email: 'igorfonseca@gmail.com',
-            password: '123456'
-        })
-        .expect(201)
+
+const user = {
+    userName: "Paula",
+    email: "paula@gmail.com",
+    password: "12346",
+};
+
+beforeEach(async () => {
+    await User.deleteMany()
+    await new User(user).save()
 })
 
-afterEach(async () => {
-    const db = mongoose.connection;
-    await db.dropDatabase()
+describe('Testar rotas de login e cadastro da API', () => {
+
+    test('Deve cadastrar usuário', async () => {
+        await request(app)
+            .post('/users/signup')
+            .send({
+                userName: 'Igor',
+                email: 'igorfonseca@gmail.com',
+                password: '123456'
+            })
+            .expect(201)
+    })
+
+
+    test('Deve fazer login usuário', async () => {
+        await request(app)
+            .post('/users/login')
+            .send({
+                email: user.email,
+                password: user.password
+            })
+            .expect(200)
+    })
+
+})
+
+afterAll(async () => {
+    await mongoose.connection.close()
 })
