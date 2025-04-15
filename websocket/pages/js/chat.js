bcaf7a1b48c4b959d321ed93a2061e9240a95dd4
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-04-15 16:49:04 -0300

    realtime chat project

--- websocket/pages/js/chat.js
+++ websocket/pages/js/chat.js
@@ -1,1 +1,17 @@
-io()
+
+const socket = io()
+
+const button = document.querySelector('#enviar')
+
+socket.on('countUpdated', (res) => {
+    console.log('Count has been updated', res)
+})
+
+const message = 'oi'
+
+button.addEventListener('click', (e) => {
+
+    console.log(e.target.value)
+
+    socket.emit('message', {})
+})
--- websocket/server.js
+++ websocket/server.js
@@ -5,21 +5,35 @@ const http = require('http')
 const { Server } = require('socket.io')
 const path = require('path')
 
-
 const app = express();
 const server = http.createServer(app)
 const io = new Server(server)
 
 const port = process.env.PORT || 5000
+app.use(express.static(path.join(__dirname, 'pages')));
 
 
-app.get('/', (req, res) => {
-    res.sendFile('chat.html', { root: path.join(__dirname, 'pages') })
-})
+let count = 0
 
-
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('Nova conexão websocket')
+
+    // Evento emitido pelo server ---> client
+    socket.emit('countUpdated', { count })
+
+
+    // Evento emitido pelo client ---> Server
+    socket.on('message', (message) => {
+        // count++
+
+        console.log(message)
+        const dado = message.message
+        // O método socket.emit() retorna somente para quem emitiu o evento
+        // socket.emit('countUpdated', { count })
+
+        // O método io.emit() retorna para todos que estão conectados ao servidor
+        io.emit('countUpdated', dado)
+    })
 })
 
 
