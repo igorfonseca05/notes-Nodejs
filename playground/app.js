3e6f7a2c88429954c458a442eff55447b6d90366
igorfonseca05 <igorfondev@gmail.com>
2025-07-25 21:05:42 -0300

    resolvendo problemas de stream no node

--- playground/app.js
+++ playground/app.js
@@ -1,9 +1,40 @@
-console.log('Starting')
+// console.log("Início");
 
-setTimeout(()=> {
-    console.log('mostrei depois de 3 segundos')
-}, 3000)
+// const travarThread = () => {
+//   console.log("Travou thread Principal");
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("Resolvida");
+//     }, 5000);
+//   });
 
+// };
 
+// await travarThread();
 
-console.log('Stopping')
+// console.log("Fim");
+
+var lengthOfLongestSubstring = function (s) {
+  if (typeof s !== "string") return;
+  if (!s) return;
+
+  let test = [];
+
+  s.split("").forEach((letter, i) => {
+    test.includes(letter)
+      ? test = [letter]
+      : test.push(letter);
+  });
+
+  return test.length;
+};
+
+// lengthOfLongestSubstring('abcabcabc')
+console.log(lengthOfLongestSubstring("abcabcabb"));
+// lengthOfLongestSubstring('bbbbb')
+console.log(lengthOfLongestSubstring("bbbbb"));
+console.log(lengthOfLongestSubstring("pwwkew"));
+
+// lengthOfLongestSubstring('pwwkew')
+
+// console.log([1,2,3].includes(3))
