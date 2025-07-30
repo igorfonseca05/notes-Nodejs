692b973ec4a5f2ab5b20b46a46d95ad7bbd12a25
igorfonseca05 <igorfondev@gmail.com>
2025-07-30 16:02:16 -0300

    Finalizei primeira parte curso nodejs avançado

---
+++ advancedNode/request.js
@@ -0,0 +1,21 @@
+
+// const https = require('https')
+
+// const start = Date.now()
+
+// function request () {
+//     https.request('https://www.google.com.br', res => {
+//     res.on('data', () => {})
+//     res.on('end', () => {
+//         console.log(Date.now() - start)
+//     })
+
+// }).end()
+// }
+
+// request()
+// request()
+// request()
+// request()
+// request()
+// request()
