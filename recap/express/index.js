3067276aebc45f6c44d10a1341d34510d0647da7
igorfonseca05 <igorfondev@gmail.com>
2025-08-26 11:06:20 -0300

    revisando req params

--- recap/express/index.js
+++ recap/express/index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
     res.send('Bem vindo ao servidor')
 })
 
+app.get('/:id', (req, res) => {
+    console.log(req.params.id)
+    res.send(`Voce enviou o parametro`, req.params.id)
+})
+
 
 app.listen(3000, () => {
     console.log('Servidor on')
